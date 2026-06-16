36ec2e9fc9c9f85636160dab10a847875424076c
Roberto Zabala <robertozabala3332@gmail.com>
2026-06-15 22:33:45 -0300

    Agrego archivo hojadenumeros

---
+++ hojadenumeros.py
@@ -0,0 +1,20 @@
+import operaciones
+
+numeros = []
+
+for i in range(5):
+    numero = int(input("Escribe un numero: "))
+    numeros.append(numero)
+
+suma = operaciones.sumar_lista(numeros)
+
+promedio = operaciones.sacar_promedio(numeros)
+
+mayor = operaciones.encontrar_mayor(numeros)
+
+print("Tu lista de numeros es:", numeros)
+print("La suma total es", suma)
+print("El promedio es", promedio)
+print("El numero mayor es", mayor)
+
+input("Presiona Enter para salir...")
